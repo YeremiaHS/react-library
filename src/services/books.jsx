e3607291e8c42b6aefc4bf8f1c9c3390e5541636
YeremiaHS <yeremiaherbert2405@gmail.com>
2023-09-28 19:52:00 +0700

    add, update, delete book service

--- src/services/books.jsx
+++ src/services/books.jsx
@@ -2,6 +2,7 @@ import { fetchApi } from "../config/api";
 
 const BASE_API_URL_DEV = import.meta.env.VITE_API_URL_DEV;
 
+// get all book
 export const getBookService = async() => {
     try {
         const url = `${BASE_API_URL_DEV}/books/available`;
@@ -20,10 +21,47 @@ export const getBookService = async() => {
     }
 };
 
-export const getBookByIdService = async() => {
+// get book by id
+export const getBookByIdService = async(id) => {
     try {
-        const Url = `${BASE_API_URL_DEV}/books/id`;
+        const Url = `${BASE_API_URL_DEV}/books/${id}`;
         const response = await fetchApi({url, method:"GET"});
+
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//  add book 
+export const addBookService = async(data) => {
+    try {
+        const url = `${BASE_API_URL_DEV}/books/add`;
+        const response = await fetchApi({ url, method:"POST", data});
+
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// update book
+export const updateBookService = async (id, data) => {
+    try {
+        const url = `${BASE_API_URL_DEV}/update/${id}`
+        const response = await fetchApi({url, method:"PUT", data})
+
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// delete book
+export const deleteBookService = async(id) => {
+    try {
+        const url = `${BASE_API_URL_DEV}/${id}`
+        const response = await fetchApi({url, method:"DELETE"});
 
         return response;
     } catch (error) {
